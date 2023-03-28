00d1e00abf82ae0784e7a41dff7e3dea40aaccc0
Josh Michielsen <github@mickey.dev>
2023-03-28 12:23:41 +0100

    add media view, update create command, register command

--- src/commands/createComponent.ts
+++ src/commands/createComponent.ts
@@ -3,6 +3,7 @@ import { output } from "../output";
 import { getExtensionContext } from "../extensionContext";
 import { CreateComponentPanel } from "../webviews/createComponent";
 import { workspace } from "vscode";
+import { workspaceTreeViewProvider } from "../views/treeViews";
 
 export async function createComponentView() {
   CreateComponentPanel.createOrShow(getExtensionContext());
@@ -19,11 +20,13 @@ export async function createComponent(
   }
 
   const targetPath = workspace.workspaceFolders[0].uri.toString().replace("file://", "");
-  const outFile = `${targetPath}/${name}`;
+  const outFile = `${targetPath}/component-archive`;
 
-  const cmd = `ocm create componentarchive ${name} ${version} --provider ${provider} --scheme ${scheme} -o ${outFile}`;
+  const cmd = `ocm create componentarchive ${name} ${version} --provider ${provider} --scheme ${scheme} -F ${outFile}`;
   output.send(`Creating component ${name}:${version} to ${outFile}...`);
 
   const result = await shell.exec(cmd);
-  output.send(`Component creation complete: ${result.stdout}`);
+  output.send(`Component creation complete: ${result.stdout || result.stderr}`);
+
+  workspaceTreeViewProvider.refresh();
 }
