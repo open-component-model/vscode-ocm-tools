44082ffa950f27c60e587615489fdb2b520a6519
Josh Michielsen <github@mickey.dev>
2023-03-29 12:40:00 +0100

    clean up output, add confirmation for overwriting existing archive

--- src/commands/createComponent.ts
+++ src/commands/createComponent.ts
@@ -2,8 +2,9 @@ import { shell } from "../shell";
 import { output } from "../output";
 import { getExtensionContext } from "../extensionContext";
 import { CreateComponentPanel } from "../webviews/createComponent";
-import { workspace } from "vscode";
 import { workspaceTreeViewProvider } from "../views/treeViews";
+import { window, workspace } from "vscode";
+import { existsSync, rmSync } from "fs";
 
 export async function createComponentView() {
   CreateComponentPanel.createOrShow(getExtensionContext());
@@ -22,11 +23,30 @@ export async function createComponent(
   const targetPath = workspace.workspaceFolders[0].uri.toString().replace("file://", "");
   const outFile = `${targetPath}/component-archive`;
 
+  if (existsSync(outFile)) {
+    const answer = await window.showWarningMessage(
+      "compent-archive already exists. Overwrite?",
+      "Yes",
+      "No"
+    );
+
+    if (answer !== "Yes") {
+      output.send(`Component creation cancelled, ${outFile} already exists.`);
+      return;
+    }
+
+    rmSync(outFile, { recursive: true, force: true });
+  }
+
   const cmd = `ocm create componentarchive ${name} ${version} --provider ${provider} --scheme ${scheme} -F ${outFile}`;
   output.send(`Creating component ${name}:${version} to ${outFile}...`);
 
   const result = await shell.exec(cmd);
-  output.send(`Component creation complete: ${result.stdout || result.stderr}`);
+  result.code !== 0
+    ? output.send(`Component creation failed: ${result.stderr}`)
+    : result.stdout || result.stderr
+    ? output.send(`Component creation complete: ${result.stdout || result.stderr}`)
+    : output.send(`Component creation complete!`);
 
   workspaceTreeViewProvider.refresh();
 }
