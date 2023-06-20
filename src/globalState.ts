9d771813d49de964405339820c7b7b1a446f40e8
Josh Michielsen <github@mickey.dev>
2023-06-20 15:00:23 +0100

    keep the previously provided repo url in add component

--- src/globalState.ts
+++ src/globalState.ts
@@ -1,53 +1,54 @@
-import { ExtensionContext, window, workspace } from 'vscode';
+import { ExtensionContext, window, workspace } from "vscode";
 
-const enum GlobalStatePrefixes {
-}
+const enum GlobalStatePrefixes {}
 
 export const enum GlobalStateKey {
-	// eslint-disable-next-line @typescript-eslint/naming-convention
-	Components = 'components',
-	// eslint-disable-next-line @typescript-eslint/naming-convention
-	FirstEverActivationStorageKey = 'firstEverActivation',
-	// eslint-disable-next-line @typescript-eslint/naming-convention
-	OCMPath = 'ocm-path',
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  Components = "components",
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  FirstEverActivationStorageKey = "firstEverActivation",
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  OCMPath = "ocm-path",
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  DefaultRepositoryURL = "default-repository-url",
 }
 
 interface GlobalStateKeyMapping {
-	[GlobalStateKey.Components]: string[];
-	[GlobalStateKey.FirstEverActivationStorageKey]: boolean;
-	[GlobalStateKey.OCMPath]: string;
+  [GlobalStateKey.Components]: string[];
+  [GlobalStateKey.FirstEverActivationStorageKey]: boolean;
+  [GlobalStateKey.OCMPath]: string;
+  [GlobalStateKey.DefaultRepositoryURL]: string;
 }
 
 export class GlobalState {
+  constructor(private context: ExtensionContext) {}
 
-	constructor(private context: ExtensionContext) {}
+  get<T extends GlobalStateKeyMapping, E extends keyof T>(stateKey: E): T[E] | undefined {
+    return this.context.globalState.get(stateKey as string);
+  }
 
-	get<T extends GlobalStateKeyMapping, E extends keyof T>(stateKey: E): T[E] | undefined {
-		return this.context.globalState.get(stateKey as string);
-	}
+  set<T extends GlobalStateKeyMapping, E extends keyof T>(stateKey: E, newValue: T[E]): void {
+    this.context.globalState.update(stateKey as string, newValue);
+  }
 
-	set<T extends GlobalStateKeyMapping, E extends keyof T>(stateKey: E, newValue: T[E]): void {
-		this.context.globalState.update(stateKey as string, newValue);
-	}
+  /**
+   * Run while developing to see the entire global storage contents.
+   */
+  async showGlobalStateValue() {
+    const document = await workspace.openTextDocument({
+      language: "jsonc",
+      // @ts-ignore
+      content: JSON.stringify(this.context.globalState._value, null, "  "),
+    });
+    window.showTextDocument(document);
+  }
 
-	/**
-	 * Run while developing to see the entire global storage contents.
-	 */
-	async showGlobalStateValue() {
-		const document = await workspace.openTextDocument({
-			language: 'jsonc',
-			// @ts-ignore
-			content: JSON.stringify(this.context.globalState._value, null, '  '),
-		});
-		window.showTextDocument(document);
-	}
-
-	/**
-	 * Dev function (clear all global state properties).
-	 */
-	clearGlobalState() {
-		for (const key of this.context.globalState.keys()) {
-			this.context.globalState.update(key, undefined);
-		}
-	}
+  /**
+   * Dev function (clear all global state properties).
+   */
+  clearGlobalState() {
+    for (const key of this.context.globalState.keys()) {
+      this.context.globalState.update(key, undefined);
+    }
+  }
 }
