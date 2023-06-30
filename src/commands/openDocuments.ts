f9a3655875702bd909c73dcaa6caabfccba14577
Josh Michielsen <github@mickey.dev>
2023-06-30 13:01:25 +0100

    add progress indicator when opening a remote file from OCM Tools (#53)

--- src/commands/openDocuments.ts
+++ src/commands/openDocuments.ts
@@ -1,15 +1,34 @@
-import { window, Uri, workspace } from 'vscode';
-import { HttpsGardenerCloudSchemasComponentDescriptorOcmV3Alpha1 as ComponentDescriptorV3, Reference, ResourceType, SourceDefinition } from '../ocm/ocmv3';
-import { ComponentMeta } from '../views/componentDescriptorToNode';
+import { ProgressLocation, Uri, window, workspace } from "vscode";
+import {
+  HttpsGardenerCloudSchemasComponentDescriptorOcmV3Alpha1 as ComponentDescriptorV3,
+  Reference,
+  ResourceType,
+  SourceDefinition,
+} from "../ocm/ocmv3";
+import { ComponentMeta } from "../views/componentDescriptorToNode";
 
-export async function openDocument(meta: ComponentMeta, elem: ResourceType | SourceDefinition | Reference | ComponentDescriptorV3, type: string): Promise<void> {
-	let source: string = meta.registry ? meta.registry : meta.path;
-	let uri: string;
-	if (type === "ComponentVersion") {
-		uri = `ocm:${source}//${meta.name}/${meta.version}.yaml?type=${type}`;
-	} else {
-		uri = `ocm:${source}//${meta.name}/${elem.name}.yaml?type=${type}`;
-	}
-	const doc = await workspace.openTextDocument(Uri.parse(uri));
-	await window.showTextDocument(doc, { preview: false });
-};
+export async function openDocument(
+  meta: ComponentMeta,
+  elem: ResourceType | SourceDefinition | Reference | ComponentDescriptorV3,
+  type: string
+): Promise<void> {
+  let source: string = meta.registry ? meta.registry : meta.path;
+  let uri: string;
+  if (type === "ComponentVersion") {
+    uri = `ocm:${source}//${meta.name}/${meta.version}.yaml?type=${type}`;
+  } else {
+    uri = `ocm:${source}//${meta.name}/${elem.name}.yaml?type=${type}`;
+  }
+  window.withProgress(
+    {
+      location: ProgressLocation.Notification,
+      cancellable: false,
+      title: `Opening remote file`,
+    },
+    async (progress) => {
+      progress.report({ message: `${uri}` });
+      const doc = await workspace.openTextDocument(Uri.parse(uri));
+      await window.showTextDocument(doc, { preview: false });
+    }
+  );
+}
